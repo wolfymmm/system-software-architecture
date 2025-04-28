dd0ccda71e438b3fe855ae711ea717ab1e0aa78a
wolfymmm <marusina.yana@lll.kpi.ua>
2025-04-28 20:48:20 +0300

    ended

--- Pr9/task9.2.c
+++ Pr9/task9.2.c
@@ -4,13 +4,11 @@
 int main() {
     FILE *fp;
     char line[256];
-    // Виконуємо команду через sudo
     fp = popen("sudo cat /etc/shadow", "r");
     if (fp == NULL) {
         perror("Failed to run command");
         return 1;
     }
-    // /etc/shadow
     while (fgets(line, sizeof(line), fp) != NULL) {
         printf("%s", line);
     }
