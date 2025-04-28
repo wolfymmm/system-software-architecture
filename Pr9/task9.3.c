3fb9873c8b2d77b233e9b4053f51e4405020adee
wolfymmm <marusina.yana@lll.kpi.ua>
2025-04-28 14:58:03 +0300

    task3

--- Pr9/task9.3.c
+++ Pr9/task9.3.c
@@ -12,27 +12,23 @@ int main() {
     const char *filename = "testfile.txt";
     const char *content = "This file was created by root.\n";
 
-    // Перевірка, що програма запущена від root
     if (getuid() != 0) {
         printf("Please run as root.\n");
         return 1;
     }
 
-    // Отримуємо ім'я звичайного користувача з sudo
     const char *normal_user = getenv("SUDO_USER");
     if (!normal_user) {
         printf("SUDO_USER not set. Run with sudo.\n");
         return 1;
     }
 
-    // Отримуємо інформацію про звичайного користувача
     struct passwd *pwd = getpwnam(normal_user);
     if (!pwd) {
         printf("User %s not found.\n", normal_user);
         return 1;
     }
 
-    // Створюємо файл у /tmp
     char src_path[PATH_MAX];
     snprintf(src_path, sizeof(src_path), "/tmp/%s", filename);
 
@@ -45,11 +41,9 @@ int main() {
     fclose(f);
     printf("Created file: %s\n", src_path);
 
-    // Копіюємо файл у домашній каталог користувача
     char dst_path[PATH_MAX];
     snprintf(dst_path, sizeof(dst_path), "%s/%s", pwd->pw_dir, filename);
 
-    // Копіювання файлу
     int src_fd = open(src_path, O_RDONLY);
     if (src_fd < 0) {
         perror("open source");
@@ -63,17 +57,18 @@ int main() {
         return 1;
     }
 
-    struct stat stat_buf;
-    fstat(src_fd, &stat_buf);
-    sendfile(dst_fd, src_fd, NULL, stat_buf.st_size);
+    char buffer[4096];
+    ssize_t bytes_read;
+    while ((bytes_read = read(src_fd, buffer, sizeof(buffer))) > 0) {
+        write(dst_fd, buffer, bytes_read);
+    }
 
     close(src_fd);
     close(dst_fd);
     printf("Copied file to: %s\n", dst_path);
 
-    // Змінюємо власника файлу на звичайного користувача
-    if (chown(dst_path, pwd->pw_uid, pwd->pw_gid) != 0) {
-        perror("chown");
+    if (chown(dst_path, pwd->pw_uid, pwd->pw_gid) == -1) {
+        perror("Failed to change owner");
         return 1;
     }
     printf("Changed owner to %s\n", normal_user);
@@ -82,7 +77,9 @@ int main() {
     printf("1. Edit the file: %s\n", dst_path);
     printf("2. Save changes\n");
     printf("3. Delete the file: rm %s\n", dst_path);
-    printf("\nWhat will happen?\n");
+    printf("\nExpected behavior:\n");
+    printf("- User CAN edit and save the file (they own it).\n");
+    printf("- User CAN delete the file (they have write permission in their home dir).\n");
 
     return 0;
 }
